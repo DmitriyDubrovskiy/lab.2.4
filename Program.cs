dc48d4ab9f649a0b565441614865e133b8914e41
DmitriyDubrovskiy <150027533+DmitriyDubrovskiy@users.noreply.github.com>
2023-12-03 17:58:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+class Program
+{
+    static void Main()
+    {
+        // Приклад використання класу User
+        User user = new User("user123", "Іван", "Іванов", 25);
+        user.DisplayInformation();
+    }
+}
